1fa8045d9e8e04044c41f144faaf84c22565d279
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2024-12-02 22:37:02 +0530

    inital setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div>
+      {/* https://www.youtube.com/watch?v=F1rWdCDEixs&ab_channel=HankTheTank @ 1:15 */}
+      react - tailwind - typescript- flash cards
+    </div>
+  );
+}
+
+export default App;
